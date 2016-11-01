054a375bc5101605db488f58e7c4d9bb3290a9d8
Michail-Okunew <michail8.ki@gmail.com>
2016-11-01 17:01:19 +0100

    Hauptmenü, toolbox, versuchsübersicht und impressum
    >Neuerungen
    Hauptmenü hinzugefügt
    Toolbox hinzugefügt
    Impressum und Nutzungsbedingungen (ohne Text) hinzugefügt
    Versuchsübersicht (ohne Text und ohne Bild von T.  Young) hinzugefügt
    >Änderungen
    keine

---
+++ UToolbox.pas
@@ -0,0 +1,24 @@
+unit UToolbox;
+
+interface
+
+implementation
+
+uses Math;
+
+const KLichtgeschwindigkeit= 299792458;                                         //Lichtgeschwindigkeit in Meter pro Sekunde
+
+//Umrechnung einer übergebenen Frequenz f in die zugehörige Wellenlänge
+function FrequenzInWellenlaenge (f:real):real;
+begin
+  result:=KLichtgeschwindigkeit/f;
+end;
+
+//Berechnung des Abstands der Maxima 0. und 1. Ordnung auf dem Schirm mit übergebenem Abstand a zwischen Schirm und Blende sowie übergebener Wellenlänge lambda
+function AbstandMaxima (a,lambda:real) :real;
+begin
+  result:= a*tan(arcsin(lambda));
+end;
+
+
+end.
