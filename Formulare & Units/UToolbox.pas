8304ec93cf2203f3a944d64f0949f0b64081f4c7
ThoJor <Jordt_Thorben@t-online.de>
2016-12-28 12:18:02 +0100

    Änderung Umlaute ersetzt UToolbox
    >Änderungen
    In der UToolbox die Umlaute ersetzt
    
    >Neuerungen

--- Formulare & Units/UToolbox.pas
+++ Formulare & Units/UToolbox.pas
@@ -11,13 +11,13 @@ uses Math;
 
 const KLichtgeschwindigkeit= 299792458;                                         //Lichtgeschwindigkeit in Meter pro Sekunde
 
-//Umrechnung einer übergebenen Frequenz f in die zugehörige Wellenlänge
+//Umrechnung einer uebergebenen Frequenz f in die zugehoerige Wellenlaenge
 function FrequenzInWellenlaenge (f:real):real;
 begin
   result:=KLichtgeschwindigkeit/f;
 end;
 
-//Berechnung des Abstands der Maxima 0. und 1. Ordnung auf dem Schirm mit übergebenem Abstand a zwischen Schirm und Blende sowie übergebener Wellenlänge lambda
+//Berechnung des Abstands der Maxima 0. und 1. Ordnung auf dem Schirm mit uebergebenem Abstand a zwischen Schirm und Blende sowie uebergebener Wellenlaenge lambda
 function AbstandMaxima (a,lambda:real) :real;
 begin
   result:= a*tan(arcsin(lambda));
