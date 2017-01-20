961a4b11439e3ef238f3c83e9aa36322153e14a5
tobiaskorb <tobias.korb@icloud.com>
2017-01-20 23:08:09 +0100

    Toolbox aufräumen

--- Formulare & Units/UToolbox.pas
+++ Formulare & Units/UToolbox.pas
@@ -26,10 +26,6 @@ end;
 //Berechnung des Abstands der Maxima, e = SchirmAbstand (Abstand Spalt-Schirm), d = SpaltAbstand, lambda = Wellenlaenge, n = Anzahl des Maxima
 function AbstandMaxima (e,d,lambda,n:real) :real;
 begin
-  //result:= a*tan(arcsin(lambda));
-  //result:=e*lambda/d
-
-
   if ((lambda*n)/d <= 1) then
     result:= e * tan(arcsin((lambda*n)/d)) Else
     result:= -1;
