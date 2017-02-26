37be0fc504c13ebbd6a30b18ceaf72dc8dfbe76a
tobiaskorb <tobias.korb@icloud.com>
2017-02-26 19:36:48 +0100

    Mann ey

--- Formulare & Units/Projektionsflaeche.pas
+++ Formulare & Units/Projektionsflaeche.pas
@@ -1194,10 +1194,9 @@ begin
   EdtSpaltanzahl.Text:='2';
   EdtSchirmAbstand.Text:='10';
 
-  EdtSpaltabstand.Text:='0'+myFormatSettings.DecimalSeparator+'1';
- { if (myFormatSettings.DecimalSeparator=#$18) then
+  if (myFormatSettings.DecimalSeparator=#$18) then
     EdtSpaltbreite.Text:='0,1' else
-    EdtSpaltbreite.Text:='0.1';  }
+    EdtSpaltbreite.Text:='0.1';
 
   //Hilfe
   Hilfe_aus;
